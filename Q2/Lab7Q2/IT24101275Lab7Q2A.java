4334a0e30abb0bcb0275e546053da586302a6486
IT24101275 <IT24101275@my.sliit.lk>
2024-09-29 10:52:53 +0530

    Add files via upload

---
+++ Q2/Lab7Q2/IT24101275Lab7Q2A.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class IT24101275Lab7Q2A
+{
+       public static void main(String[] args)
+       {
+         Scanner input = new Scanner(System.in);
+         int i;
+         int j;
+  
+         for(j = 1; j <= 4; j = j + 1)
+         {
+ 
+            for(i = 1; i <= 5; i = i + 1)
+            {
+               System.out.print("$" + " ");
+            }
+
+           System.out.println("\n");
+
+         }
+       }
+}
---
+++ Q2/Lab7Q2/IT24101275Lab7Q2C.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class IT24101275Lab7Q2C
+{
+     public static void main(String[] args)
+     {
+
+     Scanner input = new Scanner(System.in);
+     
+     int i;
+     int j;
+     
+     for(i = 5; i >= 1; i--)
+     {
+        
+        for(j = 1; j <= i; j++)
+        {
+           System.out.print(i); 
+        }
+
+         System.out.println();
+         
+     }
+   
+     }
+}
